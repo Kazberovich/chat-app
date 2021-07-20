8eb79975d920548030c7276bab6864af8e052b26
Sergey <kazbers@gmail.com>
2021-07-20 21:24:12 +0300

    delivering test pushes to iOS and Android devies

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firestore_app/widgets/chat/messages.dart';
 import 'package:flutter_firestore_app/widgets/chat/new_message.dart';
@@ -13,6 +14,37 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
     super.initState();
+
+    final fm = FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    FirebaseMessaging.onBackgroundMessage((message) {
+      print('Got a message in background!');
+      print('Message data: ${message.data}');
+      return;
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      print(event.data);
+      print(event.toString());
+    });
+
     Firebase.initializeApp().whenComplete(() {
       print("completed");
       setState(() {});
